6d89c4a7ccc80c545b1ca6c8a22f8616a0d2d789
Satish <dadassatish6@gmail.com>
2025-10-02 09:17:58 +0530

    first

---
+++ mysql/p1.sql
@@ -0,0 +1,11 @@
+-- show DATABASES;
+-- CREATE DATABASE political_parties;
+-- use political_parties;
+
+-- DROP TABLE parties;
+-- CREATE Table parties(name varchar(20),leader varchar(20), mp int,mla int);
+-- desc parties;
+insert into parties(name,leader,mp,mla)
+VALUES ("bjp","modi",20,10),
+        ("congress","gandhi",2,4),
+        ("MNS","thakre",0,3);
---
+++ p3.py
@@ -0,0 +1,4 @@
+print(10>20)
+print(30==35)
+print(25<45)
+print(10!=5)
